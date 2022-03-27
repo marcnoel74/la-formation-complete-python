248098c463c9adb70f7a98be166a72b0dbf7a1fa
marcnoel74 <vykby8-Sychaw-nofvuh>
2022-03-27 16:20:17 +0200

    Résultats de l'exercice

--- exr-001_input-utilisateur/01-sources/recuperer_input.py
+++ exr-001_input-utilisateur/01-sources/recuperer_input.py
@@ -1,2 +1,12 @@
 # Demander le prénom, la ville de naissance et l'âge de la personne qui utilise le script
 # Afficher ces trois informations
+
+first_name = input('Entrez votre prénom: ')
+city = input('Entrez votre ville: ')
+age = input('Entrez votre age: ')
+
+print(first_name)
+print(city)
+print(age)
+
+
