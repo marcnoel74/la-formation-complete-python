2c5e154867e25d5ff3ccbf440323a2703c118f61
marcnoel74 <vykby8-Sychaw-nofvuh>
2022-03-27 16:06:52 +0200

    Divers changements

--- chp-001_les-bases/afficher-le-type-dune-variable/script.py
+++ chp-001_les-bases/afficher-le-type-dune-variable/script.py
@@ -1,6 +1,6 @@
-nombre = input("Entrez un nombre: ")
-print(type(nombre))  # retourne <class 'str'>. La fonction input retourne toujours une chaîne de caractères.
+number_exo = input("Entrez un nombre: ")
+print(type(number_exo))  # retourne <class 'str'>. La fonction input retourne toujours une chaîne de caractères.
 
 # Vous pouvez par la suite convertir la variable nombre en nombre entier grâce à la fonction int
-nombre = int(nombre)
-print(type(nombre))  # retourne <class 'int'>. La variable a bien été convertie.
+number_exo = int(number_exo)
+print(type(number_exo))  # retourne <class 'int'>. La variable a bien été convertie.
