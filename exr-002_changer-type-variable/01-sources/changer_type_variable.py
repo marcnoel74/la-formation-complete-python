9426ac47aca4b8476f86bc31006a2884daab8bec
marcnoel74 <vykby8-Sychaw-nofvuh>
2022-03-27 16:30:15 +0200

    Résultats de l'exercice

--- exr-002_changer-type-variable/01-sources/changer_type_variable.py
+++ exr-002_changer-type-variable/01-sources/changer_type_variable.py
@@ -3,3 +3,6 @@
 # Par exemple : "Le résultat de l'opération est 15" (dans le cas où l'utilisateur entre le nombre 5)
 
 a = 10
+
+nombre_utilisateur = int(input('Entrez un nombre :'))
+print(f'Le résultat de l\'addition de {a} et {nombre_utilisateur} vaut : {a + nombre_utilisateur}')
